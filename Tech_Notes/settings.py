56d45e60f92a8f7b031d821b0198d0780114e43f
shawon <shawonislam121009@gmail.com>
2021-01-10 14:31:14 +0600

    changes in allowed host

--- Tech_Notes/settings.py
+++ Tech_Notes/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'jnpz5=9yk8a#v+%(erndyk)1l!mpyr@h%4!-jej6u3dm7-thw#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://tech-notes-by-shawon.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://tech-notes-shawon.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
